789b26f0519515666306195528a6c04500532929
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-06-09 17:55:23 +0300

    Vue Native App
    This Mobile App is created to open a given web page and connect to Firebase

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+    <web-view :source="{uri:'https://invoicemanager-a542d.firebaseio.com/user/name.json?access_token=${token}'}" />
+    
+</template>
+
+<script>
+import firebase from 'firebase';
+import { WebView } from "react-native-webview";
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyD92XYKrF6SbhcLoUTpjPaBUx9HxyPw",
+    authDomain: "invoicemanager-a542d.firebaseapp.com",
+    databaseURL: "https://invoicemanager-a542d.firebaseio.com",
+    projectId: "invoicemanager-a542d",
+    storageBucket: "invoicemanager-a542d.appspot.com",
+    messagingSenderId: "758688562180",
+    appId: "1:758688562180:android:19e6c4a66b124727"
+  };
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+export default {
+  name: "myComponent",
+  components: {
+    "web-view": WebView
+  },
+  methods: {
+    signInWithGoogle() {
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
+}
+
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var tokenGenerator = new FirebaseTokenGenerator("<YOUR_FIREBASE_SECRET>");
+var token = tokenGenerator.createToken({uid: "1", some: "arbitrary", data: "here"});
+
+</script>
